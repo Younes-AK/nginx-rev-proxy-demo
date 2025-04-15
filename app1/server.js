8ccc1ae9a8d194f69dbe302e11f6b9f902be9883
Younes Akazdaou <yakazdao@e3r11p2.1337.ma>
2025-04-15 11:06:13 +0100

    init commit

---
+++ app1/server.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const ID = process.env.ID || 'App';
+
+const server = http.createServer((req, res) => {
+  res.end(`Response from ${ID} on port ${PORT}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`${ID} is running on port ${PORT}`);
+});
+
